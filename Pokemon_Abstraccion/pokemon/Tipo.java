1a5796f9ac49230655e1ebc21e6881f31e072bb8
OctavioChable <145056643+OctavioChable@users.noreply.github.com>
2023-10-13 16:14:00 -0600

    Add files via upload

---
+++ Pokemon_Abstraccion/pokemon/Tipo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemon;
+
+/**
+ *
+ * @author octav
+ */
+public enum Tipo {
+    
+    ACERO, AGUA, BICHO, DRAGON, ELECTRICO, FANTASMA, FUEGO, HADA, HIELO, LUCHA, NORMAL, PLANTA, PSIQUICO, ROCA, SINIESTRO, TIERRA,VENENO,VOLADOR 
+
+}
